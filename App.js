2f8ef6e7f334709feb303a68b10c8e895a5cab90
Renan Ferreira dos Santos <rferreira@logfacil.com.br>
2018-09-12 09:35:03 -0300

    Refactor Login Screen/ Code

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import React from "react";
-import { View } from "react-native";
-import LoginScreen from "./src/components/LoginScreen/LoginScreen";
+import React from 'react';
+import { View } from 'react-native';
+import LoginScreen from './src/components/LoginScreen/LoginScreen';
+import FirebaseConfig from './src/components/constants/FirebaseConfig';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyAA9yiDCHy4-lb_tmGiQ2LSiHdrcuM3fOM',
+  authDomain: 'quizflix-649eb.firebaseapp.com',
+  databaseURL: 'https://quizflix-649eb.firebaseio.com',
+  projectId: 'quizflix-649eb',
+  storageBucket: 'quizflix-649eb.appspot.com',
+  messagingSenderId: '68936384738',
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+  console.log('Iniciou firebase');
+}
+
+//Nao esta rodando----------------
+firebase.auth().onAuthStateChanged(user => {
+  if (user != null) {
+    console.log('We are authenticated now!');
+  }
+});
+//----------------------------------
 
 export default class App extends React.Component {
   render() {
-    return (
-        <LoginScreen />
-    );
+    return <LoginScreen firebase={firebase} />;
   }
 }
